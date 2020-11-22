35e03fd22e2e97b6c801bbe9266900418aa74bde
Sam Kennerly <samkennerly@gmail.com>
2020-11-21 20:01:35 -0500

    Add timer. Make output easier to read.

--- src/picasino.cpp
+++ src/picasino.cpp
@@ -1,6 +1,14 @@
 /*
-Monte Carlo estimate of pi.
-Can only be runs interactively.
+Estimate pi.
+Run interactively.
+Use a brute-force Monte Carlo method.
+*/
+
+/* TO DO
+- don't use namespace std?
+- pass dart by reference?
+- inline functions?
+- cleanup
 */
 
 #include<iostream>
@@ -9,11 +17,11 @@ Can only be runs interactively.
 
 using namespace std;
 
-#define MILLION 1000000
 #define PI 3.14159265358979323846264338327950288419716939937510
 
 double sqradius( vector<double>& point ){
 	// Return squared radius because we don't have time for square roots.
+
 	double x = point[0];
 	double y = point[1];
 
@@ -27,33 +35,46 @@ double coord(){
 }
 
 int main(){
-	// Ask user number of samples. Print estimated pi and error.
-	vector<double> point(2);
+
+	vector<double> dart(2);
 	unsigned long tosses = 0;
 	unsigned long hits = 0;
 	double pi_ish = 0;
 
-	cout << endl << "How many million darts should we throw? ";
-	cin >> tosses;
-	tosses *= MILLION;
+	clock_t started;
+	float elapsed;
 
-	// Seed the pseudorandom number generator
+	// Seed the random number generator
 	srand(time(NULL));
 
+	// Ask user for input
+	cout << endl << "How many million darts? ";
+	cin >> tosses;
+	tosses *= 1000000;
+
+	// Throw darts
+	started = std::clock();
 	cout << "Throwing darts..." << endl;
-	for ( int d = 0; d < tosses; d++ ){
-		point[0] = coord();
-		point[1] = coord();
-		if ( sqradius(point) <= 1.0 ){
+	for (int d = 0; d < tosses; d++){
+		dart[0] = coord();
+		dart[1] = coord();
+		if (sqradius(dart) <= 1.0){
 			hits ++;
 		}
 	}
-	pi_ish = (4.0 * hits) / tosses;
+	pi_ish = 4.0 * hits / tosses;
+	elapsed = std::clock() - started;
+	elapsed /= CLOCKS_PER_SEC;
 
-	cout.precision(10);
-	cout << hits << " hits out of " << tosses << " tosses. " << endl;
-	cout << "Estimated value of pi is " << pi_ish << endl;
-	cout << "Error is " << pi_ish - PI << endl;
+	// Display results
+	cout.precision(8);
+	cout << endl;
+	cout << "Estimate: " << pi_ish << endl;
+	cout << "Error:    " << pi_ish - PI << endl;
+	cout << "Tosses:   " << tosses << endl;
+	cout << "Hits:     " << hits << endl;
+	cout << "Time:     " << elapsed << " seconds" << endl;
+	cout << endl;
 
 	return 0;
 }
