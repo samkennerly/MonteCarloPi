af824318a2a7fab6e8402df82b99bd94061ebfae
Sam Kennerly <samkennerly@gmail.com>
2020-11-21 20:29:12 -0500

    Refactor for easier reading

--- src/picasino.cpp
+++ src/picasino.cpp
@@ -5,10 +5,7 @@ Use a brute-force Monte Carlo method.
 */
 
 /* TO DO
-- don't use namespace std?
-- pass dart by reference?
-- inline functions?
-- cleanup
+- built-in PI?
 */
 
 #include<iostream>
@@ -19,30 +16,44 @@ using namespace std;
 
 #define PI 3.14159265358979323846264338327950288419716939937510
 
-double sqradius( vector<double>& point ){
-	// Return squared radius because we don't have time for square roots.
-
-	double x = point[0];
-	double y = point[1];
-
-	return x*x + y*y;
-}
-
-double coord(){
+double sample(){
 	// Random sample from uniform distribution on [-1, 1).
 
 	return 2.0 * rand() / RAND_MAX - 1.0;
 }
 
+double sqradius(vector<double> &point){
+	// Return squared radius. We don't have time for square roots.
+
+	double x = point[0];
+	double y = point[1];
+
+	return x * x + y * y;
+}
+
+double hits(unsigned long &tosses){
+	// Throw many darts. How many landed inside the circle?
+
+	unsigned long hits = 0;
+	vector<double> dart(2);
+
+	for (int d = 0; d < tosses; d++){
+		dart[0] = sample();
+		dart[1] = sample();
+		if (sqradius(dart) <= 1.0) {
+			hits++;
+		}
+	}
+
+	return hits;
+}
+
 int main(){
 
-	vector<double> dart(2);
-	unsigned long tosses = 0;
-	unsigned long hits = 0;
-	double pi_ish = 0;
-
 	clock_t started;
-	float elapsed;
+	unsigned long tosses = 0;
+	double roughpi = 0;
+	double elapsed;
 
 	// Seed the random number generator
 	srand(time(NULL));
@@ -53,27 +64,18 @@ int main(){
 	tosses *= 1000000;
 
 	// Throw darts
-	started = std::clock();
 	cout << "Throwing darts..." << endl;
-	for (int d = 0; d < tosses; d++){
-		dart[0] = coord();
-		dart[1] = coord();
-		if (sqradius(dart) <= 1.0){
-			hits ++;
-		}
-	}
-	pi_ish = 4.0 * hits / tosses;
-	elapsed = std::clock() - started;
-	elapsed /= CLOCKS_PER_SEC;
+	started = clock();
+	roughpi = 4.0 * hits(tosses) / tosses;
+	elapsed = clock() - started;
 
 	// Display results
 	cout.precision(8);
 	cout << endl;
-	cout << "Estimate: " << pi_ish << endl;
-	cout << "Error:    " << pi_ish - PI << endl;
+	cout << "Estimate: " << roughpi << endl;
+	cout << "Error:    " << roughpi - PI << endl;
 	cout << "Tosses:   " << tosses << endl;
-	cout << "Hits:     " << hits << endl;
-	cout << "Time:     " << elapsed << " seconds" << endl;
+	cout << "Time:     " << elapsed / CLOCKS_PER_SEC << " seconds" << endl;
 	cout << endl;
 
 	return 0;
